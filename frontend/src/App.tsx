18ac9021cc66ce607fe20a66ca242f8a3fcfc94d
Lamassu15 <bazo.assyr@gmail.com>
2025-05-13 17:18:51 +0200

    Bugs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import { scrollToTop } from "@/utils/scrollUtils";
 import "./App.css";
@@ -15,7 +10,6 @@ import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import { Toaster } from "@/components/ui/sonner";
 import NotFound from "./pages/NotFound";
-
 
 // ScrollToTop component to reset scroll position on route change
 const ScrollToTop = () => {
@@ -32,19 +26,19 @@ function App() {
   return (
     <>
       <Toaster position="top-right" />
-      <Router>
+      <BrowserRouter>
         <ScrollToTop />
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/Skills" element={<Skills />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/skills" element={<Skills />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
